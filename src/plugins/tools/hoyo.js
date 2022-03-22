14af2f70721158023c3ed4d204bd1e0ce2c1c086
USS-LongIsland <wz601@outlook.com>
2022-03-22 08:27:54 +0800

    updated

--- src/plugins/tools/hoyo.js
+++ src/plugins/tools/hoyo.js
@@ -1,5 +1,26 @@
+import { isValidCookie } from "#utils/cookie";
+import { filterWordsByRegex, getWordByRegex } from "#utils/tools";
+import db from "#utils/database";
 function hoyo(msg) {
-  const message = 'HelloWorld';
-  msg.bot.say(msg.sid, message, msg.type, msg.uid);
- }
-export { hoyo };
+  var ckformat = msg.uid 
+  db.init(ck);
+  const HW = 'HelloWorld';
+  const get = filterWordsByRegex(msg.text, 'ck');
+  const message = get+"\n";
+  if (get !== 'unmatch'){
+    switch (isValidCookie(get)){
+      case true:
+        msg.bot.say(msg.sid,message+HW, msg.type, msg.uid);
+        if (!db.includes(ck,msg.uid,{ cookie })){
+          db.push(ck,msg.uid,{ cookie: get })
+        } else{
+          msg.bot.say(msg.sid,'already', msg.type, msg.uid);
+        }
+        break;
+      case false:
+        msg.bot.say(msg.sid,'笨蛋吧你', msg.type, msg.uid);
+        break;
+    }
+  }
+}
+export { hoyo }
