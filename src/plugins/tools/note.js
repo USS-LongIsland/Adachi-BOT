55bcbe0ffaa5bee6e74ad9f22a5d9f913bad05b0
LongIsland <wz601@outlook.com>
2022-05-09 11:56:05 +0800

    听说你们地球24小时就叫一天

--- src/plugins/tools/note.js
+++ src/plugins/tools/note.js
@@ -32,6 +32,7 @@ function time(value) {
     var secondTime = parseInt(value);// 秒
     var minuteTime = 0;// 分
     var hourTime = 0;// 小时
+    var dayTime = 0
     if (secondTime > 60) {//如果秒数大于60，将秒数转换成整数
         //获取分钟，除以60取整数，得到整数分钟
         minuteTime = parseInt(secondTime / 60);
@@ -43,6 +44,10 @@ function time(value) {
             hourTime = parseInt(minuteTime / 60);
             //获取小时后取佘的分，获取分钟除以60取佘的分
             minuteTime = parseInt(minuteTime % 60);
+            if (hourTime > 24){
+                dayTime = parseInt(hourTime / 24)
+                hourTime = parseInt(minuteTime % 60)
+            }
         }
     }
     var time = "" + parseInt(secondTime) + "秒";
@@ -52,6 +57,9 @@ function time(value) {
     }
     if (hourTime > 0) {
         time = "" + parseInt(hourTime) + "小时" + time;
+    }
+    if (dayTime > 0){
+        time = "" + parseInt(dayTime) + "天" + time;
     }
     return time;
 }
